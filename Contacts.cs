0d6415ea5ddf939a5da5f254a5edd76f4bc67dc0
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-16 14:02:24 +0400

    time and space complexity

---
+++ Contacts.cs
@@ -0,0 +1,43 @@
+﻿namespace complexity
+{
+    internal class Contacts
+    {
+        public List<Contact> contacts;
+        public string name { get; private set; }
+
+
+        public Contacts()
+        {
+            contacts = new List<Contact>();
+        }
+
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+        public Contact GetContact(string name)
+        {
+            foreach (Contact contact in contacts)
+            {
+                if(contact.Name == name)
+                {
+                    return contact;
+                }
+            }
+            return null;
+        }
+    }
+
+    internal class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Contact(string Name, string PhoneNumber)
+        {
+            this.Name = Name;
+            this.PhoneNumber = PhoneNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace complexity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Contacts contacts = new Contacts();
+            contacts.AddContact(new Contact("Said", "28127"));
+            contacts.AddContact(new Contact("Hamed", "232323"));
+            contacts.AddContact(new Contact("Salim", "837312"));
+
+            Console.WriteLine(contacts.GetContact("Hamed"));
+        }
+    }
+}
